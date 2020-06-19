499d2143270b69909236c8f4bd7c288cc64f6d3e
jjherbas <65332463+jjherbas@users.noreply.github.com>
2020-06-19 20:23:29 +0800

    Created Vue App through CLI and added VueMapbox to package

---
+++ estates-frontend-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <MapContainerComponent msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <MapComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import MapContainerComponent from "./components/MapContainerComponent.vue";
+import MapComponent from "./components/MapComponent.vue";
+
+@Component({
+  components: {
+    MapContainerComponent,
+    MapComponent
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ estates-frontend-app/src/components/MapComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="map">
+        <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
+    </div>
+</template>
+
+<script>
+import Mapbox from "mapbox-gl";
+import { MglMap } from "vue-mapbox";
+
+export default {
+  components: {
+    MglMap
+  },
+  data() {
+    return {
+      accessToken: "pk.eyJ1IjoiampuYWQiLCJhIjoiY2tibTB6ZzBjMWI1czJycGYyMDloaHZ3eiJ9.IXe1E4jrDre_1zRtH-yGNA", // your access token. Needed if you using Mapbox maps
+      mapStyle: "mapbox://styles/mapbox/light-v10" // your map style
+    };
+  },
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
+  }
+};
+</script>
